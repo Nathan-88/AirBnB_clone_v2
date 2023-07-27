6858a763e5a36a969c3f15b8a75a5470d683dba3
Nathan-88 <onwukaebuka88@gmail.com>
2023-07-27 20:51:18 +0100

    Update the def do_create(self, arg): function of your command interpreter (console.py) to allow for object creation with given parameters

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,16 +115,37 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        # split args into an array and get the class
+        args_arr = args.split()
+        cls_ = args_arr[0]
+
+        # isolate the parameters
+        instance_attr = args_arr[1:]
+        kwargs = {}
+
+        if not cls_:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif cls_ not in self.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+
+        for attr in instance_attr:
+            # separate, remove quotes then add to kwargs dict
+            stripped = attr.split("=")
+            key = stripped[0]
+            value = stripped[1].replace('"', '')
+            if key in self.types:
+                type_func = self.types[key]
+                value = type_func(value)
+            elif value.find('_'):
+                value = value.replace('_', ' ')
+            kwargs[key] = value
+
+        new_instance = self.classes[cls_](**kwargs)
+        new_instance.save()
         print(new_instance.id)
-        storage.save()
+
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,19 +293,19 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
-                second_quote = args.find('\"', 1)
+            if args and args[0] == '"':  # check for quoted arg
+                second_quote = args.find('"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
 
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
-                att_val = args[2][1:args[2].find('\"', 1)]
+            if args[2] and args[2][0] == '"':
+                att_val = args[2][1:args[2].find('"', 1)]
 
             # if att_val was not quoted arg
             if not att_val and args[2]:
